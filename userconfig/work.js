fd7895c7293ea97072b4711f9b52ea967c635b69
ITRecords <fabrice@itrecords.fr>
2025-05-24 08:10:09 +0200

    Create work.js

---
+++ userconfig/work.js
@@ -0,0 +1,9 @@
+const CONFIG = new Config({
+  // ...
+  clock: {
+    format: 'h:i p',        // 13:30 PM
+    // format: 'do B Y - h:i', // 18th January 2021 - 13:30
+    // format: 'h:i - m/b/Y',  // 13:30 - 3/Jul/2021
+    iconColor: 'grey'
+  }
+});
